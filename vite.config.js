0ac4af1104a3018f74b16835583bcf23e1bd87bc
JoshuaMc1 <joshua15mclean@gmail.com>
2024-04-21 21:07:38 -0600

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            manifest: {
+                name: 'Personal AniList',
+                short_name: 'PAL',
+                description: 'Crea tus listas de animes y personajes favoritos con nuestra aplicación web.',
+                theme_color: '#212529',
+                icons: [
+                    {
+                        src: '/android-launchericon-144-144.png',
+                        sizes: '144x144',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-launchericon-192-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-launchericon-512-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-launchericon-512-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: '/android-launchericon-512-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                ],
+                display: 'standalone',
+                background_color: '#212529',
+                start_url: '.',
+                orientation: 'portrait',
+            }
+        })
+    ],
 })
